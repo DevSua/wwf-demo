4d555fe8ee7e19454f28a5079dfb13d55bb2df08
DevSua <tndk5321@naver.com>
2022-04-13 01:00:09 +0900

    slider && nav bar

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+//SWIPER//
 const swiper = new Swiper(".swiper-container", {
   centeredSlides: true,
-  loopAdditionalSlides: 1, //loop를 해도 끊기지않고 부드럽게 이어줌
+  loopAdditionalSlides: 1, //슬라이드 마지막에서 다음슬라이드가 보여지지않는 현상 수정
+  slidesPerView: 1,
+  loop: true,
   autoplay: {
-    loop: true,
     delay: 5000,
-    disableOnInteraction: true, //두장이 끝나고 또 재생
+    disableOnInteraction: true, //끝난후 자동재생
   },
   navigation: {
     nextEl: ".swiper-button-next",
