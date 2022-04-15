ca65fe860c6ed1b62230eaecde09a1d636978e64
DevSua <tndk5321@naver.com>
2022-04-15 23:05:39 +0900

    grid

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-//SWIPER//
-const swiper = new Swiper(".swiper-container", {
-  centeredSlides: true,
-  loopAdditionalSlides: 1, //슬라이드 마지막에서 다음슬라이드가 보여지지않는 현상 수정
-  slidesPerView: 1,
-  loop: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: true, //끝난후 자동재생
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+// //SWIPER//
+// const swiper = new Swiper(".swiper-container", {
+//   centeredSlides: true,
+//   loopAdditionalSlides: 1, //슬라이드 마지막에서 다음슬라이드가 보여지지않는 현상 수정
+//   slidesPerView: 1,
+//   loop: true,
+//   autoplay: {
+//     delay: 5000,
+//     disableOnInteraction: true, //끝난후 자동재생
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+// 847때쯤에 검은색으로
