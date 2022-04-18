6a9b40d30280c4530db380e45e818ab1b70cad9e
DevSua <tndk5321@naver.com>
2022-04-18 16:25:25 +0900

    involved

--- index.js
+++ index.js
@@ -13,5 +13,17 @@
 //     prevEl: ".swiper-button-prev",
 //   },
 // });
+// const swiper = new Swiper(".get_involved .swiper-container", {
+//   centeredSlides: true,
+//   loopAdditionalSlides: 1, //슬라이드 마지막에서 다음슬라이드가 보여지지않는 현상 수정
+//   slidesPerView: 5,
+//   loop: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+//마우스로 스크롤 할수있게 만들기
 
 // 847때쯤에 검은색으로
+//https://www.nextree.co.kr/p8622/ 반응형
